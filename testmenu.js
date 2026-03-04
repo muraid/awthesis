71a7a6716b16cc2f2f40687b0bd26863eed3eff8
wwilmajonsson <wilmajonsson2001@hotmail.com>
2026-03-04 10:29:37 +0100

    Update testmenu.js

--- testmenu.js
+++ testmenu.js
@@ -3,6 +3,22 @@ Bangle.drawWidgets();
 
 const storage = require("Storage");
 
+const config = {
+  filename: settings.filename || "mobistudy.bin",
+  samplingPeriod: settings.interval, 
+  bytesPerStepCount: 1
+};
+
+let writtenRows = 0;
+let lastTotalStepCount = -1;
+let currentStepCount = 0;
+let accelSum = 0;
+let accelSamples = 0;
+let hr = 0;
+let hrConfidence = 0;
+let isMeasuring = false;
+
+
 let settings = storage.readJSON("mobistudy.json", 1) || {
     interval: 10,
     vibration: true,
