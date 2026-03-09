cc617e0cf665caec36fcac7a48d1c287bc1c296f
muraid <80200216+muraid@users.noreply.github.com>
2026-03-09 13:44:21 +0100

    Update testmenu.js

--- testmenu.js
+++ testmenu.js
@@ -19,15 +19,6 @@ function showStartMenu() {
         "": { "title": "Start Menu" },
         "< Back": () => load(),
 
-        "Sensors": {
-            onchange: () => showSensorsMenu()
-        },
-        "Settings": {
-            onchange: () => showSettingsMenu()
-        },
-        "Participant": {
-            onchange: () => showParticipantMenu()
-        },
         "Timed tests": {
             onchange: () => showTimedTestsMenu()
         },
@@ -38,112 +29,5 @@ function showStartMenu() {
     E.showMenu(menu);
 }
 
-function showSensorsMenu() {
-    const menu = {
-        "": { "title": "Sensors" },
-        "< Back": function() { showStartMenu(); },
 
-        "Interval (s)": {
-            value: settings.interval,
-            min: 1,
-            max: 60,
-            step: 1,
-            onchange: v => {
-                settings.interval = v;
-                saveSettings();
-            }
-},
-        "Vibration": { // change so we can control time and intensity of vibration
-            value: settings.vibration,
-            onchange: v => {
-                settings.vibration = v; 
-                saveSettings();
-            }
-        },
-
-        "Accelerometer": {
-            value: settings.accelerometer,
-            onchange: v => {
-                settings.accelerometer = v; 
-                saveSettings();
-            }
-        },
-        "Gyroscope": {
-            value: settings.gyroscope,
-            onchange: v => {
-                settings.gyroscope = v; 
-                saveSettings();
-            }
-        },
-        "Heart Rate": {
-            value: settings.heartRate,
-            onchange: v => {
-                settings.heartRate = v; 
-                saveSettings();
-            }
-        }
-
-    };
-    E.showMenu(menu);
-}
-
-function showSettingsMenu() {
-    const menu = {
-        "": { "title": "Sensors" },
-        "< Back": function() { showStartMenu(); },
-
-        "Bluetooth": { //turn off ability to connect bluetooth to unknown devices, only allow connection to phone app
-            value: settings.bluetooth,
-            onchange: v => {
-                settings.bluetooth = v; 
-                saveSettings();
-            }
-},
-        "Display": { //change display settings such as brightness and timeout
-            value: settings.display,
-            onchange: v => {    
-                settings.display = v;
-                saveSettings();
-            }
-        },
-        "Sound": { //turn off sound and control volume
-            value: settings.sound,
-            onchange: v => {
-                settings.sound = v; 
-                saveSettings();
-            }
-        },
-       
-    };    E.showMenu(menu);
-}
-
-function showParticipantMenu() {
-    const menu = {
-        "": { "title": "Participant" },
-        "< Back": function() { showStartMenu(); },  
-        "ID": {
-            value: settings.participantID || "",
-            onchange: v => {
-                settings.participantID = v;
-                saveSettings();
-            }
-        },
-        "Age": {
-            value: settings.participantAge || "",
-            onchange: v => {
-                settings.participantAge = v;
-                saveSettings();
-            }
-        },  
-        "Gender": {
-            value: settings.participantGender || "",
-            onchange: v => {
-                settings.participantGender = v;
-                saveSettings();
-            }
-        }
-    };
-    E.showMenu(menu);
-}
 showStartMenu();
- showSettingsMenu();
