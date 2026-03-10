f037da3de4f7f7a6867bb3ff3042f46f9cc25bc6
muraid <80200216+muraid@users.noreply.github.com>
2026-03-10 09:49:41 +0100

    Update testmenu.js

--- testmenu.js
+++ testmenu.js
@@ -9,6 +9,8 @@ const config = {
   samplingPeriod: settings.interval, 
   bytesPerStepCount: 1
 };
+const bytesPerRow = 10;
+const totalFileLen = 28800;
 
 let writtenRows = 0;
 let lastTotalStepCount = -1;
@@ -41,14 +43,52 @@ function showStartMenu() {
         "": { "title": "Start Menu" },
         "< Back": () => load(),
 
+        "Sensors": {
+            onchange: () => showSensorMenu()
+        },
         "Timed tests": {
             onchange: () => showTimedTestsMenu()
         },
         "EMA": {
             onchange: () => showEMAMenu()
+        },
+        
+        "Start": {
+            onchange: () => startDataCollection()
         }
     };
     E.showMenu(menu);
+}
+
+function showSensorMenu(){
+
+}
+
+function showTimedTestsMenu(){
+
+}
+
+function showEMAMenu(){
+
+}
+
+function startDataCollection(){
+    let ts = Math.round(Date.now() / 1000);
+
+    let accelAvg = accelSamples ? (accelSum / accelSamples) : 0;
+    let accelByte = Math.min(255, Math.round(accelAvg * 100));
+
+    let batt = E.getBattery();   // NEW
+
+    appendRow(ts, currentStepCount, accelByte, hr, hrConfidence, batt);
+
+    // Reset values
+    currentStepCount = 0;
+    accelSum = 0;
+    accelSamples = 0;
+
+
+    measureHR();
 }
 
 
